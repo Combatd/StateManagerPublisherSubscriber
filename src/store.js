87e19049d883cb01c68bcbfb1df3b87ae865a4a9
Combatd <combatdstar@gmail.com>
2021-05-31 00:16:36 -0700

    Define Store#getState returning a shallow copy of global state

--- src/store.js
+++ src/store.js
@@ -2,7 +2,16 @@ class Store {
     constructor(rootReducer) {
         this.rootReducer = rootReducer;
         this.globalState = {
-            
+
         };
+    }
+    /*
+    #getState
+    returns a shallow copy of the global state.
+    May be refactored later to not contain pointers to the same attribute
+    values of the global state.
+    */
+    getState = function() {
+        return Object.assign(this.globalState);
     }
 }
